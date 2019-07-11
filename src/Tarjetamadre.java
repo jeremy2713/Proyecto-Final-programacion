09024c069f71b74c5233544805b185f23d63d120
Jeremy Almanzar <Jeremy Almanzar@DESKTOP-F4G06KN>
2019-07-11 19:29:10 -0400

    clase Tarjetamadre

---
+++ src/Tarjetamadre.java
@@ -0,0 +1,9 @@
+
+public class Tarjetamadre extends Componente {
+
+	public Tarjetamadre(float precio, int cantidad_disponible, String barcode, String marca, String modelo) {
+		super(precio, cantidad_disponible, barcode, marca, modelo);
+		// TODO Auto-generated constructor stub
+	}
+
+}
